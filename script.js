eb91be5691d17bc14c996875474530ab9c22fdcd
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-02 12:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-function majorityElement(arr) {
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on("line", (line) => {
+  input.push(line.trim());
+});
+
+rl.on("close", () => {
+  let n = parseInt(input[0]);
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    arr.push(Number(input[i]));
+  }
+
+  function majorityElement(arr) {
     let n = arr.length;
     let count = 1;
     let res = 0;
 
     for (let i = 1; i < n; i++) {
-        if (arr[i] === arr[res]) {
-            count++;
-        } else {
-            count--;
-        }
-        if (count === 0) {
-            res = i;
-            count = 1;
-        }
+      if (arr[i] === arr[res]) {
+        count++;
+      } else {
+        count--;
+      }
+      if (count === 0) {
+        res = i;
+        count = 1;
+      }
     }
 
     let ans = 0;
     for (let i = 0; i < n; i++) {
-        if (arr[i] === arr[res]) {
-            ans++;
-        }
+      if (arr[i] === arr[res]) {
+        ans++;
+      }
     }
 
     if (ans > Math.floor(n / 2)) {
-        return arr[res];
+      return arr[res];
     } else {
-        return -1;
+      return -1;
     }
-}
+  }
 
-let n = parseInt(prompt("Enter the value of n"));
-let arr = [];
+  console.log(majorityElement(arr));
+});
 
-for (let i = 0; i < n; i++) {
-    arr.push(Number(prompt(`Enter element ${i + 1}:`)));
-}
-
-majorityElement(arr);
 
 
